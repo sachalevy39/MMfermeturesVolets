8321693c1e658ffc19b53e7b0faabb118318e958
sachalevy39 <61126107+sachalevy39@users.noreply.github.com>
2020-11-07 23:05:32 +0100

    format and test contact form

--- php/callmeform-process.php
+++ php/callmeform-process.php
@@ -31,7 +31,7 @@ if (empty($_POST["terms"])) {
     $terms = $_POST["terms"];
 }
 
-$EmailTo = "yourname@domain.com";
+$EmailTo = "sachalevy39@gmail.com";
 $Subject = "New quote request from Aria landing page";
 
 // prepare email body text
